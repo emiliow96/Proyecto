d1c56626e0af7a0c12c69c0e67533bbcb7611b8a
admin <admin@nox.Cierva2.local>
2018-02-13 09:23:11 +0100

    practica 5 final.

--- ProyectoJV/src/JVPrincipal.java
+++ ProyectoJV/src/JVPrincipal.java
@@ -7,9 +7,9 @@
  * Se pueden detectar varios defectos y antipatrones de diseño:
  *  	- Ausencia de encapsulación.
  *  	- Clase demasiado grande. 
- * @since: prototipo1.0
+ * @since: prototipo1.1
  * @source: JVPrincipal.java 
- * @version: 1.0 - 2017/11/29
+ * @version: 1.1 - 2018/01/10
  * @author: ajp
  */
 
@@ -18,7 +18,6 @@ import java.util.Scanner;
 
 public class JVPrincipal {
 
-	// Apartado 6:
 	static final int MAX_USUARIOS = 10;
 	static final int MAX_SESIONES = 10;
 	static final int MAX_INTENTOS_FALLIDOS = 3;
@@ -27,31 +26,27 @@ public class JVPrincipal {
 	static int sesionesRegistradas = 0;				// Control de sesiones registradas.
 	static Usuario usrEnSesion;
 
-	// Apartado 8:
 	static final int TAMAÑO_MUNDO = 12;
 	static final int CICLOS_SIMULACION = 35;
 	static byte[][] espacioMundo;
-	// Apartado 8+:
 	enum FormaEspacio { PLANO1, PLANO2, ESFERICO }
 	static final FormaEspacio TIPO_MUNDO = FormaEspacio.ESFERICO;
 
 	/**
 	 * Secuencia principal del programa.
 	 */
-	public static void main(String[] args) {	
-		// Apartados 4 y 5: Pruebas previas
-		//probarUsuario();
+	public static void main(String[] args) {		
+		// Apartados 2 y 3: Pruebas 
+		//probarClaseUsuario();
 		//probarSesionUsuario();
 
-		// Apartado 7: 
 		cargarUsuariosPrueba();		
 		mostrarTodosUsuarios();
 
-		// Apartado 8:
 		if (inicioSesionCorrecto()) {
 			registrarSesion();	
 			System.out.println("Sesión: " + sesionesRegistradas + '\n' + "Iniciada por: " 
-					+ 	usrEnSesion.nombre + " " + usrEnSesion.apellidos);	
+					+ 	usrEnSesion.getNombre() + " " + usrEnSesion.getApellidos());	
 			cargarMundoDemo();
 			arrancarSimulacion();
 		}
@@ -62,7 +57,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8:
 	 * Ejecuta una simulación del juego de la vida en la consola.
 	 */
 	static void arrancarSimulacion() {
@@ -78,7 +72,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Actualiza el estado del Juego de la Vida.
 	 * Actualiza según la configuración establecida para la forma del espacio.
 	 */
@@ -95,7 +88,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8:
 	 * Despliega en la consola el estado almacenado, corresponde
 	 * a una generación del Juego de la vida.
 	 */
@@ -110,7 +102,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Actualiza el estado almacenado del Juego de la Vida.
 	 * Las celdas periféricas son adyacentes con las del lado contrario.
 	 * El mundo representado sería esférico cerrado sin límites para células de dos dimensiones.
@@ -207,7 +198,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene el estado o valor de la celda vecina situada al Oeste de la indicada por la coordenada. 
 	 * @param fila de la celda evaluada.
 	 * @param col de la celda evaluada.
@@ -221,7 +211,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene el estado o valor de la celda vecina situada al Suroeste de la indicada por la coordenada. 
 	 * @param fila de la celda evaluada.
 	 * @param col de la celda evaluada.
@@ -235,7 +224,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene el estado o valor de la celda vecina situada al Sur de la indicada por la coordenada. 
 	 * @param fila de la celda evaluada.
 	 * @param col de la celda evaluada.
@@ -249,7 +237,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene el estado o valor de la celda vecina situada al Sureste de la indicada por la coordenada. 
 	 * @param fila de la celda evaluada.
 	 * @param col de la celda evaluada.
@@ -263,7 +250,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene el estado o valor de la celda vecina situada al Este de la indicada por la coordenada. 
 	 * @param fila de la celda evaluada.
 	 * @param col de la celda evaluada.
@@ -277,7 +263,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene el estado o valor de la celda vecina situada al Noreste de la indicada por la coordenada. 
 	 * @param fila de la celda evaluada.
 	 * @param col de la celda evaluada.
@@ -291,7 +276,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene el estado o valor de la celda vecina situada al NO de la indicada por la coordenada. 
 	 * @param fila de la celda evaluada.
 	 * @param col de la celda evaluada.
@@ -305,7 +289,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene el estado o valor de la celda vecina situada al Noroeste de la indicada por la coordenada. 
 	 * @param fila de la celda evaluada.
 	 * @param col de la celda evaluada.
@@ -319,7 +302,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Actualiza el estado almacenado del Juego de la Vida.
 	 * Las celdas periféricas son los límites absolutos.
 	 * El mundo representado sería plano cerrado con límites para células de dos dimensiones.
@@ -350,7 +332,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8+:
 	 * Obtiene una submatriz con las celdas adyacentes a cualquier celda del mundo.
 	 * @param fila de la celda central de la submatriz
 	 * @param col de la celda central de la submatriz
@@ -434,7 +415,6 @@ public class JVPrincipal {
 
 
 	/**
-	 * Apartado 8:
 	 * Carga datos demo en la matriz que representa el mundo. 
 	 */
 	static void cargarMundoDemo() {
@@ -456,7 +436,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8:
 	 * Controla el acceso de usuario.
 	 * @return true si la sesión se inicia correctamente.
 	 */
@@ -468,13 +447,19 @@ public class JVPrincipal {
 			// Pide usuario y contraseña.
 			System.out.print("Introduce el nif de usuario: ");
 			String nif = teclado.nextLine();
-			System.out.print("Introduce clave acceso: ");
+			System.out.print("Introduce clavapellidose acceso: ");
 			String clave = teclado.nextLine();
 
 			// Busca usuario coincidente con las credenciales.
 			usrEnSesion = buscarUsuario(nif);
 
-			if (usrEnSesion != null && usrEnSesion.claveAcceso.equals(clave)) {
+			// Encripta clave tecleada utilizando un objeto temporal
+			// que ejecutará automáticamente el método privado.
+			Usuario aux = new Usuario();
+			aux.setClaveAcceso(clave);
+			clave = aux.getClaveAcceso();
+			
+			if (usrEnSesion != null && usrEnSesion.getClaveAcceso().equals(clave)) {
 				return true;
 			} else {
 				intentosPermitidos--;
@@ -487,7 +472,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8:
 	 * Busca usuario dado su nif.
 	 * @param idUsr - el nif del Usuario a buscar.
 	 * @return - el Usuario encontrado o null si no existe.
@@ -495,7 +479,7 @@ public class JVPrincipal {
 	static Usuario buscarUsuario(String idUsr) {
 		// Busca usuario coincidente con la credencial.
 		for (Usuario usr : datosUsuarios) {
-			if (usr.nif.equalsIgnoreCase(idUsr)) {
+			if (usr.getNif().equalsIgnoreCase(idUsr)) {
 				return usr;	// Devuelve el usuario encontrado.
 			}
 		}
@@ -503,13 +487,12 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 8:
 	 * Registro de la sesión de usuario.
 	 */
 	static void registrarSesion() {
 		SesionUsuario sesion = new SesionUsuario();
-		sesion.usr = usrEnSesion;
-		sesion.fecha = new Date().toString();
+		sesion.setUsr(usrEnSesion);
+		sesion.setFecha(new Date().toString());  // Hoy
 
 		// Registra sesion de usuario a partir de la última posición ocupada.
 		datosSesiones[sesionesRegistradas] = sesion;  
@@ -517,7 +500,6 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 7:
 	 * Muestra por consola todos los usuarios almacenados.
 	 */
 	static void mostrarTodosUsuarios() {
@@ -527,139 +509,85 @@ public class JVPrincipal {
 	}
 
 	/**
-	 * Apartado 7:
 	 * Genera datos de prueba válidos dentro 
 	 * del almacén de datos.
 	 */
-	static void cargarUsuariosPrueba() {
-		Usuario usuarioAux = new Usuario();
-		usuarioAux.nif = "2344556K";
-		usuarioAux.nombre = "Pepe"; 
-		usuarioAux.apellidos = "López Pérez";
-		usuarioAux.domicilio = "C/Luna, 27 30132 Murcia";
-		usuarioAux.correo = "pepe@gmail.com";
-		usuarioAux.fechaNacimiento = "1999.11.12";
-		usuarioAux.fechaAlta = "2017.12.3";
-		usuarioAux.claveAcceso = "Miau#0";
-		usuarioAux.rol = "usuario normal";
-		datosUsuarios[0] = usuarioAux;
-
-		for (int i = 1; i < MAX_USUARIOS; i++) {
-			usuarioAux = new Usuario();
-			usuarioAux.nif = i + "344556K";
-			usuarioAux.nombre = "Pepe" + i; 
-			usuarioAux.apellidos = "López" + " Pérez" +i ;
-			usuarioAux.domicilio = "C/Luna, 27 30132 Murcia";
-			usuarioAux.correo = "pepe" + i + "@gmail.com";
-			usuarioAux.fechaNacimiento = "1999.11.12";
-			usuarioAux.fechaAlta = "2017.12.3";
-			usuarioAux.claveAcceso = "Miau#" + i;
-			usuarioAux.rol = "usuario normal";
-			datosUsuarios[i] = usuarioAux;
+	static void cargarUsuariosPrueba() {	
+		for (int i = 0; i < MAX_USUARIOS; i++) {
+			datosUsuarios[i] = new Usuario("1234567" + i + "K", "Pepe", "López Pérez", "C/ Luna 27 30132 Murcia",
+					"pepe" + i + "@gmail.com", "1999.11.12", "2018.01.03", "Miau#" + i, Usuario.ROLES[0]);
 		}
 	}
 
 	/**
-	 * Apartado 5: 
 	 * Pruebas de la clase SesionUsuario
 	 */
 	static void probarSesionUsuario() {
 
 		// Datos para la prueba...
-		Usuario usr = new Usuario();
-		usr.nif = "23456790K";
-		usr.nombre = "Pepe";
-		usr.apellidos = "López Pérez";
-		usr.domicilio = "C/Luna, 27 30132 Murcia";
-		usr.correo = "pepe@gmail.com";
-		usr.fechaNacimiento = "1999.11.12";
-		usr.fechaAlta = "2017.12.3";
-		usr.claveAcceso = "Miau#0";
-		usr.rol = "usuario normal";
+		Usuario usr = new Usuario("23456790K", "Pepe", "López Pérez",
+				"C/ Luna 27 30132 Murcia", "pepe@gmail.com",  "1999.11.12",
+				"2018.12.03", "miau", "usuario normal");
 
 		// Prueba de la clase SesionUsuario
 		SesionUsuario sesion1 = new SesionUsuario();
-		sesion1.usr = usr;
-		sesion1.fecha = "2017.12.3";
+		sesion1.setUsr(usr);
+		sesion1.setFecha("2018.01.23");
 		System.out.println(sesion1);	
 	}
 
 	/**
-	 * Apartado 4: 
+	 * Apartado 2:
 	 * Pruebas de la clase Usuario
 	 */
-	static void probarUsuario() {	// Busca usuario coincidente con las credenciales.
-		Scanner teclado = new Scanner(System.in);
+	static void probarClaseUsuario() {	
 
-		// Prueba de la clase Usuario
+		// Prueba de constructores
 
-		Usuario usr1;
-		//Equivalencia
-		//usr1.nombre = "Luis";
-		//null.nombre = "Luis";	
-
-		// Asignación entre referencias -no duplica objeto-
-		Usuario usr2 = new Usuario();
-		usr1 = usr2;
-
-		usr2.nif = "23456790K";
-		usr2.nombre = "Pepe";
-		usr2.apellidos = "López Pérez";
-		usr2.domicilio = "C/Luna, 27 3usrSesion0132 Murcia";
-		usr2.correo = "pepe@gmail.com";
-		usr2.fechaNacimiento = "1999.11.12";
-		usr2.fechaAlta = "2017.12.3";
-		usr2.claveAcceso = "miau";
-		usr2.rol = "usuario normal";
-
-		// Modifica también usr2, son el mismo objeto
-		usr1.nombre = "Luis";
-		System.out.println("usr1: " + usr1.nombre);
-		System.out.println("usr2: " + usr2.nombre);
-
-		// Así si duplica
-		Usuario usr4 = new Usuario();
-		usr4 .nif = usr2.nif;
-		usr4.nombre = usr2.nombre;
-		usr4.apellidos = usr2.apellidos;
-		usr4.domicilio =  usr2.domicilio;
-		usr4.correo = usr2.correo;
-		usr4.fechaNacimiento = usr2.fechaNacimiento;
-		usr4.fechaAlta = usr2.fechaAlta;
-		usr4.fechaAlta = usr2.fechaAlta;
-		usr4.rol = usr2.rol;
-
-		// Son diferentes objetos.
-		usr4.nombre = "Pedro";
-		System.out.println(usr2.nombre);
-
-		// Desde teclado...
-		Usuario usr3 = new Usuario();
-		System.out.println("Entrada de datos de usuario... ");
-		System.out.print("nif: ");	
-		usr3.nif = teclado.next();
-		System.out.print("nombre: ");	
-		usr3.nombre = teclado.next();
-		System.out.print("apellidos: ");
-		usr3.apellidos = teclado.next();
-		System.out.print("domicilio: ");
-		usr3.domicilio =  teclado.next();
-		System.out.print("correo: ");
-		usr3.correo = teclado.next();
-		System.out.print("fechaNacimiento: ");
-		usr3.fechaNacimiento = teclado.next();
-		System.out.print("fechaAlta: ");
-		usr3.fechaAlta = teclado.next();
-		System.out.print("claveAcceso: ");
-		usr3.claveAcceso = teclado.next();
-		System.out.print("rol: ");
-		usr3.rol = "NORMAL";
-
-		// Si toString() de Usuario no está redefinido...
-		System.out.println(usr1); 		// Muestra identificador único de objeto
+		Usuario usr1 = new Usuario();
+		System.out.println("constructor por defecto");
+		System.out.println(usr1);
+		
+		Usuario usr2 = new Usuario("23456790K", "Pepe", "López Pérez",
+		"C/ Luna 27 30132 Murcia", "pepe@gmail.com",  "1999.11.12",
+		"2018.01.03", "miau", "usuario normal");
+		System.out.println("constructor convencional");
 		System.out.println(usr2);
-		System.out.println(usr3);
-		System.out.println(usr4);
+
+		Usuario usr3 = new Usuario(usr2);
+		System.out.println("\nconstructor copia");
+		System.out.println(usr3 != usr2);
+		System.out.println(usr3.getNif() != usr2.getNif());
+		System.out.println(usr3.getNombre() != usr2.getNombre());
+		
+		// ...
+		
+		// Prueba de métodos get...()
+		System.out.println("\nget...()");
+		System.out.println(usr2.getNif().equals("12345678K"));
+		
+		// ...
+		
+		// Prueba de métodos set...()
+		System.out.println("\nset...()");
+		usr1.setNif("12345678K");
+		System.out.println(usr1.getNif().equals("12345678K"));
+		
+		//...
+		
+		// Prueba de método privado generIdUsr()
+		System.out.println("\ngenerarIdUsr()");
+		Usuario usr4 = new Usuario("12345678K", "José", "Pérez Pérez",
+		"C/Luna, 27 30132 Murcia", "pepe@gmail.com",  "1999.11.12",
+		"2018.12.03", "miau", "usuario normal");
+		System.out.println(usr4.getIdUsr().equals("JPP8K"));
+		
+		// Prueba de método privado encriptarCesar()
+		System.out.println("\nencriptarCesar()");
+		Usuario usr5 = new Usuario("12345678K", "José", "Pérez Pérez",
+		"C/Luna, 27 30132 Murcia", "pepe@gmail.com",  "1999.11.12",
+		"2018.12.03", "Hola3", "usuario normal");
+		System.out.println(usr5.getClaveAcceso().equals("Lsoe7"));
 	}
 
 } //class
